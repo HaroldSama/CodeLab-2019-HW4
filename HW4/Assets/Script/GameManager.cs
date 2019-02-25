f3a17cf9cc07fbc7f2511ecc24271a7ca01ca641
177sma <howerds4my@gmail.com>
2019-02-25 15:34:48 -0500

    Class is Over!

---
+++ HW4/Assets/Script/GameManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Text scoreText;
+    private int score;
+
+    public Text highScoreText;
+    private int highScore;
+
+    private const string PLAYER_PREF_HIGHSCORE = "highScore";
+    private const string FILE_HIGH_SCORE = "/MyHighScoreFile.txt";
+
+    public int HighScore
+    {
+        get { return highScore; }
+        set
+        {
+            if (value > highScore)
+            {
+                highScore = value;
+                highScoreText.text = "High Score: " + highScore;
+                //PlayerPrefs.SetInt(PLAYER_PREF_HIGHSCORE, highScore);
+
+                Debug.Log(Application.dataPath);
+                string fullPathToFile = Application.dataPath + FILE_HIGH_SCORE;
+
+                /*if (File.Exists(fullPathToFile))
+                {*/
+                    File.WriteAllText(fullPathToFile, "HighScore: " + highScore);
+                    /*Debug.Log("The File Does Exist");
+                }
+                else
+                {
+                    Debug.Log("The File Doesn't Exist");
+                }*/
+            }
+        }
+    }
+
+    public int Score
+    {
+        get
+        {
+            return score;
+        }
+
+        set
+        {
+            score = value;
+            scoreText.text = "Score: " + score;
+            HighScore = score;
+        }
+    }
+
+    public static GameManager instance;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        //HighScore = PlayerPrefs.GetInt(PLAYER_PREF_HIGHSCORE, 10);
+
+        print(File.ReadAllText(Application.dataPath + FILE_HIGH_SCORE));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
